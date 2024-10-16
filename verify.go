677034aa812637d82c8b622dfdfe439de32618e5
Guido Zuidhof <me@guido.io>
2024-10-16 16:53:54 +0200

    Close request body, add `NewVerifyResult` constructor to help with mocking/testing

--- verify.go
+++ verify.go
@@ -8,6 +8,18 @@ import (
 	"net/http"
 )
 
+// NewVerifyResult creates a new VerifyResult with the given response, status code, strict mode and error.
+// This is generally only useful if you want to create a VerifyResult manually for testing purposes.
+func NewVerifyResult(response VerifyResponse, status int, strict bool, err error) VerifyResult {
+	return VerifyResult{
+		Success:  response.Success,
+		Status:   status,
+		response: response,
+		strict:   strict,
+		err:      err,
+	}
+}
+
 // VerifyCaptchaResponse takes a captcha response and verifies it with the Friendly Captcha API.
 // It returns a VerifyResult, which contains the result of the verification.
 //
@@ -34,10 +46,11 @@ func (frc *Client) VerifyCaptchaResponse(ctx context.Context, captchaResponse st
 		result.err = fmt.Errorf("%w: %v", ErrCreatingVerificationRequest, err)
 		return result
 	}
+	defer req.Body.Close()
 
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("X-Api-Key", frc.APIKey)
-	req.Header.Set("X-Frc-Sdk", fmt.Sprintf("friendly-captcha-go-sdk@%s", Version))
+	req.Header.Set("X-Frc-Sdk", fmt.Sprintf("friendly-captcha-go@%s", Version))
 
 	resp, err := frc.HTTPClient.Do(req)
 	if err != nil {
