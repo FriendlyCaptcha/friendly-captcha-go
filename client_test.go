0273ac186af2f45c8b507007d6af41fcb4e64c6a
Guido Zuidhof <me@guido.io>
2026-02-03 12:41:21 +0100

    Add support for Risk Intelligence in siteverify response

--- client_test.go
+++ client_test.go
@@ -31,9 +31,16 @@ type TestCase struct {
 	Name        string `json:"name"`
 	Response    string `json:"response"`
 	Expectation struct {
-		ShouldAccept bool `json:"should_accept"`
+		ShouldAccept    bool `json:"should_accept"`
+		WasAbleToVerify bool `json:"was_able_to_verify"`
+		IsClientError   bool `json:"is_client_error"`
 	} `json:"expectation"`
-	Strict bool `json:"strict"`
+	Strict             bool            `json:"strict"`
+	SiteverifyResponse json.RawMessage `json:"siteverify_response"`
+}
+
+type SuccessSiteverifyResponse struct {
+	Data VerifyResponseData `json:"data"`
 }
 
 func loadTestCasesFromServer() (TestCasesFile, error) {
@@ -90,6 +97,72 @@ func TestSDKWithMockServer(t *testing.T) {
 				shouldAccept,
 				fmt.Sprintf("Expected shouldAccept to be: %v, got: %v", expectedShouldAccept, shouldAccept),
 			)
+
+			assert.Equal(
+				t,
+				test.Expectation.WasAbleToVerify,
+				result.WasAbleToVerify(),
+				fmt.Sprintf("Expected WasAbleToVerify to be: %v, got: %v", test.Expectation.WasAbleToVerify, result.WasAbleToVerify()),
+			)
+
+			assert.Equal(
+				t,
+				test.Expectation.IsClientError,
+				result.IsErrorDueToClientError(),
+				fmt.Sprintf("Expected IsClientError to be: %v, got: %v", test.Expectation.IsClientError, result.IsErrorDueToClientError()),
+			)
+
+			if result.Success {
+				var expectedResponse SuccessSiteverifyResponse
+				err := json.Unmarshal(test.SiteverifyResponse, &expectedResponse)
+				if err != nil {
+					t.Fatalf("Failed to unmarshal expected siteverify response: %v", err)
+				}
+
+				exp := expectedResponse.Data
+				res := result.response.Data
+
+				assert.Equal(
+					t,
+					exp.EventID,
+					res.EventID,
+					"Event ID does not match expected value",
+				)
+
+				assert.Equal(
+					t,
+					exp.Challenge,
+					res.Challenge,
+					"Challenge data does not match expected value",
+				)
+
+				assert.Equal(
+					t,
+					exp.RiskIntelligence,
+					res.RiskIntelligence,
+					"Risk Intelligence data does not match expected value",
+				)
+
+				// Check two specific fields:
+				assert.Equal(
+					t,
+					exp.RiskIntelligence.V.Client.HeaderUserAgent,
+					res.RiskIntelligence.V.Client.HeaderUserAgent,
+				)
+				assert.Equal(
+					t,
+					exp.RiskIntelligence.V.Client.Browser.V.ID,
+					res.RiskIntelligence.V.Client.Browser.V.ID,
+				)
+
+				if exp.RiskIntelligence.Valid {
+					assert.Contains(
+						t,
+						string(res.RiskIntelligenceRaw.V),
+						"header_user_agent",
+					)
+				}
+			}
 		})
 	}
 }
--- wire.go
+++ wire.go
@@ -1,6 +1,11 @@
 package friendlycaptcha
 
-import "time"
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/guregu/null/v6"
+)
 
 // VerifyRequest is the request body for the /api/v2/captcha/siteverify endpoint. As a user of the SDK
 // you generally don't need to create this struct yourself, instead you should use the Client's methods.
@@ -20,7 +25,49 @@ type VerifyResponseChallengeData struct {
 
 // VerifyResponseData is the data found in the data field of a VerifyResponse.
 type VerifyResponseData struct {
+	// EventID is a unique identifier for this siteverify call.
+	EventID string `json:"event_id"`
+
+	// Challenge contains information about the challenge that was solved.
 	Challenge VerifyResponseChallengeData `json:"challenge"`
+
+	// RiskIntelligenceRaw contains risk information about the solver of the captcha.
+	// This may be `null` if risk intelligence is not enabled for your Friendly Captcha account.
+	//
+	// Note this is the raw JSON data, you probably want to use the RiskIntelligence field instead. This field is
+	// available in case you need to access fields that are not yet modeled in the SDK.
+	RiskIntelligenceRaw null.Value[json.RawMessage] `json:"risk_intelligence"`
+
+	// RiskIntelligence contains risk information about the solver of the captcha.
+	// This may be `null` if risk intelligence is not enabled for your Friendly Captcha account.
+	RiskIntelligence null.Value[RiskIntelligenceData] `json:"-"`
+}
+
+// UnmarshalJSON implements custom JSON unmarshaling for VerifyResponseData.
+// It automatically populates the RiskIntelligence field from RiskIntelligenceRaw.
+func (v *VerifyResponseData) UnmarshalJSON(data []byte) error {
+	// Use an auxiliary struct to avoid infinite recursion
+	type Alias VerifyResponseData
+	aux := &struct {
+		*Alias
+	}{
+		Alias: (*Alias)(v),
+	}
+
+	if err := json.Unmarshal(data, &aux); err != nil {
+		return err
+	}
+
+	// Populate RiskIntelligence from RiskIntelligenceRaw
+	if v.RiskIntelligenceRaw.Valid {
+		var riskData RiskIntelligenceData
+		if err := json.Unmarshal(v.RiskIntelligenceRaw.V, &riskData); err != nil {
+			return err
+		}
+		v.RiskIntelligence = null.ValueFrom(riskData)
+	}
+
+	return nil
 }
 
 // VerifyResponseError is the data found in the error field of a VerifyResponse in case of an error.
