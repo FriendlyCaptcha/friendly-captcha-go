73ded30404e51c55dbb15a4818379044ac01b812
Rok Sušnik <rokostik@gmail.com>
2026-03-03 20:33:04 +0100

    add token missing error

--- errors.go
+++ errors.go
@@ -39,6 +39,8 @@ const (
 	ErrorCodeSitekeyInvalid ErrorCode = "sitekey_invalid"
 	// (400) The response field is missing in your request.
 	ErrorCodeResponseMissing ErrorCode = "response_missing"
+	// (400) The risk intelligence token is missing in your request.
+	ErrorCodeTokenMissing ErrorCode = "token_missing"
 	// (400) The risk intelligence token has expired.
 	ErrorCodeTokenExpired ErrorCode = "token_expired"
 
